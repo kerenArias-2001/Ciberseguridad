6fae8c79bf7e9c4e77f5d618b3930dc5b10881f0
KerenArias-2001 <kearias79@misena.edu.co>
2021-11-29 15:54:41 -0500

    CAMBIOS- tarde

--- Ciberseguridad/datos/inc_cargo.php
+++ Ciberseguridad/datos/inc_cargo.php
@@ -13,17 +13,9 @@
         <meta name="author" content="">
 
         <title>Incidentes | Ciberseguridad</title>
-
-        <!-- Bootstrap Core CSS -->
         <link href="../css/bootstrap.min.css" rel="stylesheet">
-
-        <!-- MetisMenu CSS -->
         <link href="../css/metisMenu.min.css" rel="stylesheet">
-
-        <!-- Custom CSS -->
         <link href="../css/startmin.css" rel="stylesheet">
-    
-       <!-- Estilos de el modal -->
         <link rel="stylesheet" href="../css/style.css">
         <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.1/css/all.css" integrity="sha384-50oBUHEmvpQ+1lW4y57PTFmhCaXp0ML5d60M1M7uH2+nqUivzIebhndOJK28anvf" crossorigin="anonymous">
 
